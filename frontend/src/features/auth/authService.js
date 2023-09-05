5fff77a45b27be455635f4f221479f7bee3323c0
PhilippeMallari <mallari.philippe@gmail.com>
2023-09-05 10:13:04 -0600

    added user login functionality

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,11 +1,22 @@
 import axios from 'axios'
 
 // url endpoint for registering users // same as postman backend url 
-const API_URL = '/api/users'
+const API_URL = '/api/users/'
 
 // Register user
 const register = async (userData) => {
     const response = await axios.post(API_URL, userData)
+
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
+// Login user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
 
     if (response.data) {
         localStorage.setItem('user', JSON.stringify(response.data))
@@ -21,7 +32,8 @@ const logout = () => {
 
 const authService = {
     register,
-    logout
+    logout,
+    login
 }
 
 export default authService
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { FaSignInAlt } from "react-icons/fa"
 import {toast} from 'react-toastify'
+import { useNavigate } from "react-router-dom"
 import {useSelector, useDispatch} from 'react-redux'
-import {login} from '../features/auth/authSlice'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from "../components/Spinner"
 
 function Login() {
 
@@ -14,9 +16,23 @@ function Login() {
     const {email, password} = formData
 
     const dispatch = useDispatch()
+    const navigate = useNavigate();
 
     // This should match the one in your state, so in our case it's auth but if we use tickets, it should be 'state.tickets'
-    const { user, isLoading, isSuccess, message } = useSelector( (state) => state.auth)
+    const { user, isLoading, isSuccess, message, isError } = useSelector( (state) => state.auth)
+
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        }
+
+        // Redirect when logged in
+        if(isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset())
+    }, [isError, isSuccess, user, message, navigate, dispatch]);
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -34,6 +50,10 @@ function Login() {
             }
 
         dispatch(login(userData))
+    }
+
+    if (isLoading) {
+        return <Spinner />
     }
 
     return (
